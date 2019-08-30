48f30ebd991ef2515154944ab39c429e8c97bb22
Vaughn Iverson <vsivsi@yahoo.com>
2019-08-29 17:22:53 -0700

    Sum first element too

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func HammingDistSlice64(b1, b2 []uint64) (sum int) {
 // HammingDist4Array64Unroll counts the number bitwise differences between
 // n-bit signatures b1 and b2 passed as len 4 arrays of uint64 values.
 func HammingDist4Array64Unroll(b1, b2 *[4]uint64) (sum int) {
-	sum = bits.OnesCount64(b1[0] ^ b2[0])
+	sum += bits.OnesCount64(b1[0] ^ b2[0])
 	sum += bits.OnesCount64(b1[1] ^ b2[1])
 	sum += bits.OnesCount64(b1[2] ^ b2[2])
 	sum += bits.OnesCount64(b1[3] ^ b2[3])
