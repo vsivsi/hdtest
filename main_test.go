a0ec603eb82651f44147c11bfe7289783c96b792
Vaughn Iverson <vsivsi@yahoo.com>
2019-08-29 17:31:54 -0700

    Shorten up the benchmark labels

--- main_test.go
+++ main_test.go
@@ -12,18 +12,18 @@ func generateRandomSig() (sig [4]uint64) {
 	return sig
 }
 
-func BenchmarkHammingDist(b *testing.B) {
+func BenchmarkHD(b *testing.B) {
 
 	sigA := generateRandomSig()
 	sigB := generateRandomSig()
 
-	b.Run("uint64 256-bit unrolled array Hamming distance", func(b *testing.B) {
+	b.Run("256-bit unrolled array HD", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			HammingDist4Array64Unroll(&sigA, &sigB)
 		}
 	})
 
-	b.Run("uint64 256-bit Slice Hamming distance", func(b *testing.B) {
+	b.Run("256-bit slice HD", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			HammingDistSlice64(sigA[:], sigB[:])
 		}
